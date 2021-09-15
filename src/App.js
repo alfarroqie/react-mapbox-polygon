63333b26d3c5b7098a64de55209e4fa2e790997c
alfarroqie <48423485+alfarroqie@users.noreply.github.com>
2021-09-16 02:38:20 +0700

    polygon fill and outline with geojson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import 'antd/dist/antd.css'
+import { Card } from 'antd';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-function App() {
+import jawaTimurGeoJson from './data/jawa-timur.geojson'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hvY29vcmVvIiwiYSI6ImNrdDgxZG5ibzB4dGkycGxqZmU0YnNuMzEifQ.smJZQqkcsSI_Su9WCxbQvQ'
+
+export default function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(112.9277);
+  const [lat, setLat] = useState(-7.7432);
+  const [zoom, setZoom] = useState(7.7);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [lng, lat],
+    zoom: zoom
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+    setLng(map.current.getCenter().lng.toFixed(4));
+    setLat(map.current.getCenter().lat.toFixed(4));
+    setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+  //polygon
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('load', () => {
+      map.current.addSource('jatim', {
+        'type': 'geojson',
+        'data': jawaTimurGeoJson
+      });
+      map.current.addLayer({
+        'id': 'outline-cities',
+        'type': 'line',
+        'source': 'jatim',
+        'layout': {},
+        'paint': {
+        'line-color': '#000',
+        'line-width': 0.5
+        }
+      });
+      map.current.addLayer({
+        'id': 'fill-cities',
+        'type': 'fill',
+        'source': 'jatim', // reference the data source
+        'layout': {},
+        'paint': {
+          'fill-color': 'red', // red color fill
+          'fill-opacity': 0.1
+        },
+      });
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div className="sidebar">
+      Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
     </div>
+    <Card bordered={true}>
+      <div ref={mapContainer} className="map-container" />
+    </Card>
+  </div>
   );
-}
-
-export default App;
+};
